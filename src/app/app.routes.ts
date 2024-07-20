d67d3d21a6e09311b40955f5f1528e0ed689791f
Jeeban Giri <jeebangiri942@gmail.com>
2024-07-20 09:12:32 +0545

    add a routes for two component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { AuthComponent } from './auth/auth.component';
+import { UserComponent } from './user/user.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'auth-component', component: AuthComponent },
+  { path: 'user-component', component: UserComponent },
+];
