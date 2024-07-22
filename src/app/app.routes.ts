719380cfe4f722cd74e54dec1dac1cfbe096a537
Jeeban Giri <jeebangiri942@gmail.com>
2024-07-22 21:26:03 +0545

    add navbar to the hoem page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,7 @@ import { AuthComponent } from './auth/auth.component';
 import { UserComponent } from './user/user.component';
 
 export const routes: Routes = [
+  
   { path: 'auth-component', component: AuthComponent },
   { path: 'user-component', component: UserComponent },
 ];
