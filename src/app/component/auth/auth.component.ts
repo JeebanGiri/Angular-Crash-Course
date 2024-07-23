ec91ec0cdd5badfcf89e0eb4203e66137f1e6ddc
Jeeban Giri <jeebangiri942@gmail.com>
2024-07-23 20:42:54 +0545

    add login form

--- src/app/component/auth/auth.component.ts
+++ src/app/component/auth/auth.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-auth',
   standalone: true,
   imports: [],
   templateUrl: './auth.component.html',
-  styleUrl: './auth.component.css'
+  styleUrl: './auth.component.css',
 })
-export class AuthComponent {
-
-}
+export class AuthComponent {}
