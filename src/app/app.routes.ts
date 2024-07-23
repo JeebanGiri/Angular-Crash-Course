6b1f46975f9975f123cc637d2a28980f37489527
Jeeban Giri <jeebangiri942@gmail.com>
2024-07-23 20:18:01 +0545

    add updated code

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,8 @@
 import { Routes } from '@angular/router';
-import { AuthComponent } from './auth/auth.component';
-import { UserComponent } from './user/user.component';
+import { AuthComponent } from './component/auth/auth.component';
+import { UserComponent } from './component/user/user.component';
 
 export const routes: Routes = [
-  
   { path: 'auth-component', component: AuthComponent },
   { path: 'user-component', component: UserComponent },
 ];
