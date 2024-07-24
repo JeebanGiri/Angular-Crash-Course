cff0e1f6899ba284cd924651b6df957e495a7bf0
Jeeban Giri <jeebangiri942@gmail.com>
2024-07-24 09:13:13 +0545

    add data binding for forms

--- src/app/component/auth/auth.component.ts
+++ src/app/component/auth/auth.component.ts
@@ -1,11 +1,27 @@
 import { Component } from '@angular/core';
-import { FormBuilder } from '@angular/forms';
+import { FormBuilder, ReactiveFormsModule, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-auth',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './auth.component.html',
   styleUrl: './auth.component.css',
 })
-export class AuthComponent {}
+export class AuthComponent {
+  loginForm: FormGroup;
+  constructor(private formBuilder: FormBuilder) {
+    this.loginForm = this.formBuilder.group({
+      username: '',
+      password: '',
+    });
+  }
+  onSubmit(): void {
+    // Handle form submission
+    if (this.loginForm.valid) {
+      console.log('Form Submitted', this.loginForm.value);
+    } else {
+      console.log('Form is not valid');
+    }
+  }
+}
